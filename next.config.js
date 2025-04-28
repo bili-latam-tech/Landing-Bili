247017faefc2ffa08dd89e952a1335d778fe12d8
Geisson19 <gs.ponce20@uniandes.edu.co>
2025-04-27 22:27:16 -0500

    deploy

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/Landing-Bili' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/Landing-Bili/' : '',
+  trailingSlash: true
 }
 
 module.exports = nextConfig 
